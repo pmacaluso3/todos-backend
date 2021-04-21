1afef429bc2eec4fd15553fbcdf87d777c864380
pmacaluso3 <moxious.manoeuvre@gmail.com>
2021-04-21 11:22:34 -0400

    seed and start script

--- seeders/20210421145249-seed-todos.js
+++ seeders/20210421145249-seed-todos.js
@@ -4,15 +4,35 @@ const models = require('../models')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    
+    await models.todo.bulkCreate([
+      {
+        description: "water the plants",
+        completed: true
+      },
+      {
+        description: "feed the dogs",
+        completed: false
+      },
+      {
+        description: "take the dog to the vet",
+        completed: false
+      },
+      {
+        description: "plant a tree",
+        completed: false
+      },
+      {
+        description: "start my tax return",
+        completed: false
+      },
+      {
+        description: "apply to startup",
+        completed: true
+      },
+    ])
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await models.todo.destroy({ where: {} })
   }
 };
