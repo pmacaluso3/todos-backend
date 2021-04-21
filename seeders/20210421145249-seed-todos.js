77c3835235cc43c3ff649cb76c991db17ff0e0cd
pmacaluso3 <moxious.manoeuvre@gmail.com>
2021-04-21 11:02:21 -0400

    server

---
+++ seeders/20210421145249-seed-todos.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const models = require('../models')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+const models = require('./models')
+
+app.get('/', (req, res, next) => {
+  res.locals.data = { message: 'ok' }
+  next()
+})
+
+app.get('/todos', (req, res, next) => {
+  models.todo.findAll().then((todos) => {
+    res.locals.data = { todos }
+    next()
+  })
+})
+
+app.use((req, res) => {
+  res.json(res.locals.data)
+})
+
+app.use((error, req, res, next) => {
+  if (res.headersSent) { next(err) }
+  res.status(500).json({ error })
+})
+
+app.listen(process.env.PORT || 3001, () => {
+  console.log('server started');
+})
